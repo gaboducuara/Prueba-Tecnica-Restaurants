41160cb03275c79f277995fa7ca59f8cca0bcb59
gabriel <gabrielmancilladucuara@gmail.com>
2023-05-06 10:26:49 -0500

    actualizacion

--- routes/Search.js
+++ routes/Search.js
@@ -1,10 +1,18 @@
 const { Router } = require('express');
 const  { Search }  = require('../controllers/Search')
 
+// middlewares
+const { validarCampos } = require('../middlewares/validar-campos');
+/// -------> DB - Validators
+const { ExistName } = require("../helpers/db-validator");
+const { check } = require('express-validator');
+
 const router = Router();
 
 // ---- BUSCAR UN restaurante por ID y nombre
-
-router.get('/:colleccion/:termino', Search)
+router.get('/:colleccion/:termino', [
+    check('name', 'El campo nombre es obligatorio').not().isEmpty().custom( ExistName ),
+    validarCampos,
+] , Search)
 
 module.exports = router;
